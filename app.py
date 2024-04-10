debecd1160af9da5742b0d85f138b5fca4b1ea9c
Filip Casey <filip.r.casey@gmail.com>
2024-04-10 12:25:13 -0600

    initial api attempt; open api validation action

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
+import pandas as pd
+import json
+from snowflake.connector.pandas_tools import write_pandas
+import snowflake.connector
+from dotenv import dotenv_values
 
 app = Flask(__name__)
-
+config = dotenv_values(".env")
 
 @app.route("/")
 def status():
@@ -13,10 +18,27 @@ def status():
 
 @app.route("/verification", methods=['POST'])
 def verify():
+    rows = []
     if request.is_json:
         data = request.json
-        # parse data
-        # send to database
+        input_data = data["input"]
+        input_data["job_id"] = data["job_id"]
+        for modified_classification in data['modified_classifications']:
+            classification_data = data["modified_classifications"][modified_classification]
+            classification_data["modified_classification"] = modified_classification
+            rows.append(dict(input_data, **classification_data))
+        df = pd.DataFrame.from_dict(rows)
+        df.columns = map(str.upper, df.columns)
+        cnx = snowflake.connector.connect(
+            user=config["SNOWFLAKE_USER"],
+            password=config["SNOWFLAKE_PASSWORD"],
+            account=config["SNOWFLAKE_ACCOUNT"],
+            warehouse=config["SNOWFLAKE_WAREHOUSE"],
+            database=config["SNOWFLAKE_DATABASE"],
+            schema=config["SNOWFLAKE_SCHEMA"],
+            role=config["SNOWFLAKE_ROLE"]
+        )
+        success, nchunks, nrows, _ = write_pandas(cnx, df, "VERIFICATION_RESULTS")
         return jsonify({"message": "data received"}), 200
     else:
         return jsonify({"error": "Request body is invalid"}), 400
